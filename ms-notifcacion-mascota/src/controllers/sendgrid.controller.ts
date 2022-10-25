27a080874e041fba061b7893e5313c9eae6a9712
DannyMovilla <daniel.movilla@solinces.com>
2022-10-25 18:28:18 -0500

    init
    init

---
+++ ms-notifcacion-mascota/src/controllers/sendgrid.controller.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// Uncomment these imports to begin using these cool features!
+
+import {post, requestBody} from '@loopback/rest';
+import {Mail} from '../models';
+const sgMail = require('@sendgrid/mail');
+
+// import {inject} from '@loopback/core';
+
+export class SendgridController {
+  constructor() {}
+
+  @post('/mail', {
+    responses: {
+      '200': {
+        description: 'Login Mascota Feliz',
+      },
+    },
+  })
+  async mail(@requestBody() datamail: Mail) {
+    sgMail.setApiKey(
+      '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',
+    );
+    const msg = {
+      to: datamail.to,
+      from: 'pcns.gzkai50@myxu.info',
+      subject: datamail.subject,
+      //text: datamail.text,
+      html: datamail.text,
+    };
+
+    return sgMail.send(msg, (error: any, result: any) => {
+      if (error) {
+        return error;
+      } else {
+        return result;
+      }
+    });
+  }
+}
